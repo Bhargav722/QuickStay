b565aa30189c8f05a3f580298b42608266e93cfa
Bhargav Patil <bhargavpatil722@gmail.com>
2025-06-11 13:59:03 +0530

    Room Details included and resolve a bug in all rooms

--- src/pages/AllRooms.jsx
+++ src/pages/AllRooms.jsx
@@ -57,7 +57,7 @@ const AllRooms = () => {
                     className='max-h-65 md:w-1/2 rounded-x1 shadow-lg object-cover cursor-pointer'/>
                     <div className='md:w-1/2 flex flex-col gap-2'>
                         <p className='text-gray-500'>{room.hotel.city}</p>
-                        <p onClick={()=> {navigate(`/rooms/${roomsDummyData._id}`);scrollTo(0,0)}} className='text-gray-800 text-3x1 font-playfair cursor-pointer'>{room.hotel.name}</p>
+                        <p onClick={()=> {navigate(`/rooms/${room._id}`);scrollTo(0,0)}} className='text-gray-800 text-3x1 font-playfair cursor-pointer'>{room.hotel.name}</p>
                         <div className='flex items-center'>
                             <StarRating />
                             <p className='ml-2'>200+ reviews</p>                      
